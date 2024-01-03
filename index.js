6d7a12e64a6e4fa8e50b0c177539fd3a2a777fa4
sahalmahin <samirsatter0142@gmail.com>
2024-01-03 19:46:30 +0600

    error ditected & resolved in server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.dygd3dy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,6 +54,12 @@ app.post('/carts', async (req, res) => {
     res.send(result);
 })
 
+app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  })
 
 
 async function run() {
